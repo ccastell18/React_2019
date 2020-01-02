fa76cb83ee51a6a48815761f30b78825293b6392
Christopher Castellano <c.castellano18@gmail.com>
2020-01-02 00:44:43 -0600

    shop component

---
+++ ecommerce/clothing_app/src/pages/shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Shop_Data from './shop.data';
+
+import Collection from '../../components/collection/collection.component';
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: Shop_Data
+
+    }
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {
+          collections.map(({ id, ...otherCollectionProps }) => (
+            <Collection key={id} {...otherCollectionProps} />
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default ShopPage;
