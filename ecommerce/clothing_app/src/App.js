ce96a7d4f46a420d26f36b5247702b204827138c
Christopher Castellano <c.castellano18@gmail.com>
2020-01-04 00:33:33 -0600

    email authentication

--- ecommerce/clothing_app/src/App.js
+++ ecommerce/clothing_app/src/App.js
@@ -29,6 +29,7 @@ class App extends React.Component {
             currentUser: snapShot.id,
             ...snapShot.data()
           })
+          console.log(this.state)
         });
       }
       this.setState({ currentUser: userAuth })
