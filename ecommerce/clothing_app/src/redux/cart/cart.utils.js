9d6484fa598c221e6de41a93d841a3c256be109d
Christopher Castellano <c.castellano18@gmail.com>
2020-01-08 00:31:48 -0600

    cart item add

--- ecommerce/clothing_app/src/redux/cart/cart.utils.js
+++ ecommerce/clothing_app/src/redux/cart/cart.utils.js
@@ -6,5 +6,5 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
       : cartItem
     )
   }
-  return [cartItems, { ...cartItemToAdd, quantity: 1 }]
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
