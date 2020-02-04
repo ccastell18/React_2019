ee4eec3f6dc7ad36c593dea8d79af458bdc384f8
Christopher Castellano <c.castellano18@gmail.com>
2020-02-03 22:38:00 -0600

    saving user to database collection

--- ecommerce/crown-clothing/src/App.js
+++ ecommerce/crown-clothing/src/App.js
@@ -7,7 +7,7 @@ import Header from '../src/components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,9 +21,8 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user)
+    auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user);
     })
   }
 
