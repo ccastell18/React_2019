185728fbf3b4a2b0f2ec50433b3c1a36712ad2f9
Christopher Castellano <c.castellano18@gmail.com>
2020-02-06 00:54:33 -0600

    shop redux

--- ecommerce/crown-clothing/src/pages/shop/shop.component.jsx
+++ ecommerce/crown-clothing/src/pages/shop/shop.component.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import SHOP_DATA from './shop.data.js'
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 import './shop.styles.scss';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    {
+      collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))
     }
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </div>
-    )
-  }
-}
+  </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+
+export default connect(mapStateToProps)(ShopPage);
