064a7d406bac5c3eb50f1a8e7709e0c274f9d646
Christopher Castellano <c.castellano18@gmail.com>
2020-02-07 02:39:36 -0600

    collection page

--- ecommerce/crown-clothing/src/pages/shop/shop.component.jsx
+++ ecommerce/crown-clothing/src/pages/shop/shop.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+
+import CollectionPage from '../collection/collection.component';
+
 import './shop.styles.scss';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <CollectionOverview />
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
