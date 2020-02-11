1ec7bf0b6d56f5d870bbbb2e23ff89a246444ae2
Christopher Castellano <c.castellano18@gmail.com>
2020-02-11 00:54:58 -0600

    styled components

--- ecommerce/crown-clothing/src/components/header/header.component.jsx
+++ ecommerce/crown-clothing/src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -11,35 +10,36 @@ import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import { HeaderContainer, LogoContainer, OptionLink, OptionsContainer, OptionDiv } from './header.styles'
+
 const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link to='/' className='logo-container'>
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className='logo' />
-    </Link>
-    <div className="options">
-      <Link to='/shop' className='option'>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>
         SHOP
-      </Link>
-      <Link to='/contact' className='option'>
+      </OptionLink>
+      <OptionLink to='/contact'>
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ? (
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
         )
           :
           (
-            <Link className='option' to='/signin'>SIGN IN</Link>
+            <OptionLink to='/signin'>SIGN IN</OptionLink>
           )}
       <CartIcon />
-    </div>{
+    </OptionsContainer>{
       hidden ? null :
         <CartDropdown />
     }
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = state => createStructuredSelector({
