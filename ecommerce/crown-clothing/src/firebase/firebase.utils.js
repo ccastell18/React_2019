6c68060051d0dd16b11f2499be6eec5027d45afe
Christopher Castellano <c.castellano18@gmail.com>
2020-02-12 22:50:18 -0600

    retrieving data from database

--- ecommerce/crown-clothing/src/firebase/firebase.utils.js
+++ ecommerce/crown-clothing/src/firebase/firebase.utils.js
@@ -39,21 +39,32 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
-}
+};
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
   const collectionRef = firestore.collection(collectionKey);
-  console.log(collectionRef);
 
   const batch = firestore.batch();
   objectsToAdd.forEach(obj => {
-    const newDocRef = collectionRef.doc()
+    const newDocRef = collectionRef.doc();
     batch.set(newDocRef, obj);
   });
   return await batch.commit()
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
 
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  console.log(transformCollection)
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
