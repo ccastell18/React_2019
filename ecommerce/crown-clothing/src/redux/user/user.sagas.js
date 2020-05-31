4ced4c98aebc18017328d28053714e56ba10b864
Christopher Castellano <c.castellano18@gmail.com>
2020-05-31 16:57:24 -0500

    saga complete

--- ecommerce/crown-clothing/src/redux/user/user.sagas.js
+++ ecommerce/crown-clothing/src/redux/user/user.sagas.js
@@ -18,9 +18,9 @@ import {
   getCurrentUser,
 } from "../../firebase/firebase.utils";
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
   try {
-    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userRef = yield call(createUserProfileDocument, userAuth, additionalData);
     const userSnapshot = yield userRef.get();
     yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
   } catch (error) {
